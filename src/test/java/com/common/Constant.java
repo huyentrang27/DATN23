e94a4c7090bbe29200e974324ff89d78f9d6f2fc
Huyen Trang <huyentrang27092000@gmail.com>
2022-11-06 20:27:50 +0700

    init project

---
+++ src/test/java/com/common/Constant.java
@@ -0,0 +1,7 @@
+package com.common;
+
+public class Constant {
+    //url path
+    public static final String ALLFILMS_URL = "http://allfilm.mediadnnb.codes:3000/";
+    public static String EMAIL = "autotestallfilms@gmail.com";
+}
---
+++ src/test/java/com/utility/DataFaker.java
@@ -0,0 +1,28 @@
+package com.utility;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DataFaker {
+
+    public static String generateRandomEmail(String email){
+        if(email.contains("@")){
+            String[] parts = email.split("@");
+            return parts[0] + "+" + generatetimeStampString("MMddHHmmssSSS")+ "@" + parts[1];
+        }
+        else {
+            throw new IllegalArgumentException("The string email does't contain @");
+        }
+    }
+
+    public static String generatetimeStampString(String pattern){
+        DateTimeFormatter df = DateTimeFormatter.ofPattern(pattern);
+        LocalDateTime now = LocalDateTime.now();
+        return df.format(now);
+    }
+
+    public static String generateUserName(){
+        return "test_" + generatetimeStampString("MMddHHmmssSSS");
+    }
+
+}
