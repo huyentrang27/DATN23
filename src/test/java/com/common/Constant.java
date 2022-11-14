5b0dd4f380505eef5d505de08602f843cb57ec86
Huyen Trang <huyentrang27092000@gmail.com>
2022-11-14 22:23:40 +0700

    TC01 Implement Reigster function

--- src/test/java/com/common/Constant.java
+++ src/test/java/com/common/Constant.java
@@ -3,7 +3,8 @@ package com.common;
 public class Constant {
     //url path
     public static final String RAILWAY_URL = "http://www.raillog.somee.com";
-    public static String EMAIL_ADDRESS = "trang@gmail.com";
+    public static final String RAILWAY_HOMEPAGE_URL = "http://www.raillog.somee.com/Page/HomePage.cshtml";
+    public static String EMAIL_ADDRESS = "tnguyen@gmail.com";
     public static String PASSWORD = "Test@123456";
     public static String CONFIRM_PASSWORD = "Test@123456";
     public static String PID = "123456789";
